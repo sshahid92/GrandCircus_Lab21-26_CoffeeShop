cf2d8f27b605ae44828699a81283a02684662ca4
Shah Shahid <shahshahid@quickenloans.com>
2018-11-27 15:39:32 -0500

    Added Rigistration Controller and User class model

---
+++ GrandCircus_Lab21-26_CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrandCircus_Lab21_26_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrationComplete()
+        {
+            return View();
+        }
+    }
+}
