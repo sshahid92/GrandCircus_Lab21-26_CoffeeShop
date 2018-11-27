301a0f4f717b6ec604d3ccaedfb98d06055dfea3
Shah Shahid <shahshahid@quickenloans.com>
2018-11-27 13:42:21 -0500

    First commit

---
+++ GrandCircus_Lab21-26_CoffeeShop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GrandCircus_Lab21_26_CoffeeShop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GrandCircus_Lab21-26_CoffeeShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GrandCircus_Lab21_26_CoffeeShop.MvcApplication" Language="C#" %>
